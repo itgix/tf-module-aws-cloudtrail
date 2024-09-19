7b785b1f3c67eac14df3cda3d8c75fbd3a7c5d41
Boris Yakimov <boris.yakimov@itgix.com>
2024-09-19 10:22:13 +0300

    #139 fix resource references

--- cloudtrail.tf
+++ cloudtrail.tf
@@ -14,9 +14,9 @@ resource "aws_cloudtrail" "itgix_primary" {
   count                      = var.cloudtrail_organization_security_account ? 1 : 0
   name                       = var.cloudtrail_name
   is_organization_trail      = var.is_organization_trail
-  s3_bucket_name             = aws_s3_bucket.s3_bucket.bucket
+  s3_bucket_name             = aws_s3_bucket.itgix_cloudtrail_primary[0].bucket
   enable_log_file_validation = var.enable_log_file_validation
   cloud_watch_logs_group_arn = "${aws_cloudwatch_log_group.itgix_primary_cloudtrail[0].arn}:*"
-  cloud_watch_logs_role_arn  = aws_iam_role.ct_cw_iam_role.arn
-  kms_key_id                 = aws_kms_key.cloudtrail_kms.arn
+  cloud_watch_logs_role_arn  = aws_iam_role.cloudtrail_access_to_cloudwatch[0].arn
+  kms_key_id                 = aws_kms_key.cloudtrail_kms[0].arn
 }
