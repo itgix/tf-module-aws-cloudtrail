d68cdbdb7012b678286373f050f30fc99b80a017
Boris Yakimov <boris.yakimov@itgix.com>
2024-09-24 15:46:28 +0300

    #139 add output param for cw iam role

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,7 @@ output "cloudtrail_s3_kms_arn" {
 output "cloudtrail_cw_log_group" {
   value = try(aws_cloudwatch_log_group.itgix_primary_cloudtrail[0].arn, null)
 }
+
+output "cloudtrail_cw_iam_role" {
+  value = try(aws_iam_role.cloudtrail_access_to_cloudwatch[0].arn, null)
+}
