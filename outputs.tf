a201a1412222fda9de43344464174e509c1a2fda
Boris Yakimov <boris.yakimov@itgix.com>
2024-09-30 17:22:14 +0300

    #139 update outputs

--- outputs.tf
+++ outputs.tf
@@ -6,10 +6,10 @@ output "cloudtrail_s3_kms_arn" {
   value = try(aws_kms_key.cloudtrail_kms[0].arn, null)
 }
 
-output "cloudtrail_cw_log_group" {
+output "cloudtrail_cw_log_group_arn" {
   value = try(aws_cloudwatch_log_group.itgix_primary_cloudtrail[0].arn, null)
 }
 
-output "cloudtrail_cw_iam_role" {
+output "cloudtrail_cw_iam_role_arn" {
   value = try(aws_iam_role.cloudtrail_access_to_cloudwatch[0].arn, null)
 }
